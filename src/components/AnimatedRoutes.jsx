33b28b3a876c6c634c1bcb605af62daf7f498abb
Shubh Patel <shubhpatel6001@gmail.com>
2023-12-08 02:41:46 +0530

    5 Changes

--- src/components/AnimatedRoutes.jsx
+++ src/components/AnimatedRoutes.jsx
@@ -1,6 +1,6 @@
 // AnimatedRoutes.jsx
 import React from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import Home from '../Pages/Home';
 import Skills from '../Pages/Skills';
@@ -13,14 +13,16 @@ function AnimatedRoutes() {
 
   return (
     <AnimatePresence>
+      <Router>
       <Routes location={location} key={location.pathname}>
         {/* Set the home page as the default page using the "index" property */}
-        <Route path="/" element={<Home />} index />
+        <Route path="/" element={<Home />} exact />
         <Route path="/Home" element={<Home />} />
         <Route path="/Project" element={<Project />} />
         <Route path="/Skills" element={<Skills />} />
         <Route path="/Brief" element={<Brief />} />
       </Routes>
+      </Router>
     </AnimatePresence>
   );
 }
